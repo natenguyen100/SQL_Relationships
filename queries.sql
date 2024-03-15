fc7d575e123cdfc3bf4fd19452be5252ba093242
natenguyen100 <147363234+natenguyen100@users.noreply.github.com>
2024-03-15 18:28:14 -0500

    first commit

---
+++ queries.sql
@@ -0,0 +1,7 @@
+-- write your queries here
+
+SELECT * FROM owners FULL OUTER JOIN vehicles ON owners.id = vehicles.id;
+
+SELECT first_name, last_name, COUNT(owner_id) FROM owners JOIN vehicles on owners.id=vehicles.owner_id GROUP BY (first_name, last_name) ORDER BY first_name;
+
+SELECT first_name, last_name, ROUND(AVG(price)) as average_price, COUNT(owner_id) FROM owners JOIN vehicles on owners.id=vehicles.owner_id GROUP BY (first_name, last_name) HAVING COUNT(owner_id) > 1 AND ROUND(AVG(price)) > 10000 ORDER BY first_name DESC;
